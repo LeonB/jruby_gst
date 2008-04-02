c44a1b1e5e1252f97514c752fb51bf67be7911eb
LeonB <leon@tim-online.nl>
2008-04-02 10:10:46 +0200

    Added jruby_gst.rb... still learning git\!

--- jruby_gst.rb
+++ jruby_gst.rb
@@ -1,18 +1,25 @@
 include Java
 require '/home/leon/src/gstreamer-java/trunk/dist/gstreamer-java.jar'
 
-$: << File.expand_path(File.dirname(__FILE__) + "/lib")
-
 module JavaGst
   include_package 'org.gstreamer'
 end
 
-require 'gst'
-require 'element'
-require 'pipeline'
-require 'bin'
-require 'playbin'
-require 'bus'
-require 'message'
-require 'messagetype'
-require 'elementfactory'
+libdir = File.expand_path(File.dirname(__FILE__) + "/lib")
+
+required_files = [
+  'gst',
+  'element',
+  'pipeline',
+  'bin',
+  'playbin',
+  'bus',
+  'message',
+  'messagetype',
+  'elementfactory',
+  #'activesupport'
+]
+
+require 'rubygems'
+require 'activesupport'
+required_files.each { |file| load "#{libdir}/#{file}.rb" }
